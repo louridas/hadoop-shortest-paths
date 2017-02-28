5d9175ce4f0e1f07351ea39123bda2473f74494a
Panos Louridas <louridas@gmail.com>
2017-02-28 15:50:52 +0200

    work for strings and README update

--- AdjacencyListCreator.java
+++ AdjacencyListCreator.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,8 +12,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class AdjacencyListCreator {
 
     public static class EdgeMapper
-        extends Mapper<Object, Text, LongWritable, LongWritable>{
+        extends Mapper<Object, Text, Text, Text>{
 
+        @Override
         public void map(Object key, Text value, Context context)
             throws IOException, InterruptedException {
             String line = value.toString();
@@ -22,22 +22,23 @@ public class AdjacencyListCreator {
                 return;
             }
             String[] nodes = line.split("\\s+");
-            LongWritable source = new LongWritable(Long.parseLong(nodes[0]));
-            LongWritable dest = new LongWritable(Long.parseLong(nodes[1]));
+            Text source = new Text(nodes[0]);
+            Text dest = new Text(nodes[1]);
             context.write(source, dest);
         }
     }
 
     public static class AdjacencyListReducer
-        extends Reducer<LongWritable, LongWritable, LongWritable, Text> {
+        extends Reducer<Text, Text, Text, Text> {
 
-        public void reduce(LongWritable node,
-            Iterable<LongWritable> neighbours,
+        @Override
+        public void reduce(Text node,
+            Iterable<Text> neighbours,
             Context context) throws IOException, InterruptedException {
             StringBuffer adjacencyList = new StringBuffer();
             Text result = new Text();
 
-            for (LongWritable neighbour : neighbours) {
+            for (Text neighbour : neighbours) {
                 adjacencyList.append(neighbour);
                 adjacencyList.append(",");
             }
@@ -56,8 +57,7 @@ public class AdjacencyListCreator {
         job.setJarByClass(AdjacencyListCreator.class);
         job.setMapperClass(EdgeMapper.class);
         job.setReducerClass(AdjacencyListReducer.class);
-        job.setMapOutputValueClass(LongWritable.class);
-        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Hadoop Shortest Paths
+
+A minimal Hadoop implementation of the single-source shortest paths
+problem. The solution follows (not slavishly) the algorithm described
+in Jimmy Lin and Chris Dyer, "Data-Intensive Text Processing with
+MapReduce", published by Morgan and Claypool, 2010. For a detailed
+explanation of how the algorithm works, check the original
+publication. The following is a brief overview of the implementation
+choices.
+
+## Input Format
+
+The shortest paths algorithm expects the graph in an adjacency list
+representation. If this is not the case, the class
+[`AdjacencyListCreator`](AdjacencyListCreator.java) can be used to
+convert a graph from an "edge by line" format to an "adjacency list by
+line" format. That is, given a graph represented in files whose lines
+are:
+```
+FROM TO
+```
+`AdjacencyListCreator` converts the graph to a file with lines of the
+form:
+```
+NODE NEIGHBOUR1,NEIGHOUR2,...
+```
+
+`AdjacencyList` creator ignores lines starting with `#`, assuming that
+they are comments. As a separator for the input records it uses a
+series of whitespace characters. The name of a node is a string, but
+it may not contain whitespace, or commas.
+
+
